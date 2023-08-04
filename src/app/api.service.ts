1ead5dac106c528ff9d455d2312d886d8bd541cc
Rumen Raenkov <rumen_raenkov@abv.bg>
2023-08-04 12:27:53 +0300

    Insurance check integration

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,44 +7,121 @@ import { Subscription } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService implements OnDestroy {
-  
+
   sub!: Subscription;
-  
+
   constructor(private http: HttpClient) { }
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
 
   private getTickets(): any {
     return this.http.get(`api-iag/search.cgi`);
   };
 
   private getFullData(id: string) {
-    return this.http.get(`api-iag/get.cgi?lng=bg&session=000&user_id=0&ticket_id=${id}&digest=xxxn`)  
+    return this.http.get(`api-iag/get.cgi?lng=bg&session=000&user_id=0&ticket_id=${id}&digest=xxxn`)
   }
 
   getFullTickets() {
     return this.getTickets()
-    .pipe(
-      map((data: any) => data.records),
-      mergeAll(), 
-      mergeMap((t: any) =>
-        this.getFullData(t.id).pipe(map((data) => ({ ...t, ...data })))              
-      ),
-      take(5),
-      toArray()
-    )
+      .pipe(
+        map((data: any) => data.records),
+        mergeAll(),
+        mergeMap((t: any) =>
+          this.getFullData(t.id).pipe(map((data) => ({ ...t, ...data })))
+        ),
+        take(5),
+        toArray()
+      )
 
   }
 
   storeTickets(id: string, data: any) {
     this.sub = this.http.put(
-        `https://elemental-shine-380103-default-rtdb.europe-west1.firebasedatabase.app/e_tickets/${id}.json`,
-        { ...data }).subscribe()
-        
+      `https://elemental-shine-380103-default-rtdb.europe-west1.firebasedatabase.app/e_tickets/${id}.json`,
+      { ...data }).subscribe()
+
+  }
+
+  getInsurance(vehicleNumber: string) {
+    const body = { dkn: this.cyrlat(vehicleNumber), rama: "", stiker: "", seria: "", date: "04/08/2023", send: "търси" };
+    return this.http.post(
+      `api-ins`,
+      new URLSearchParams(body),
+      { headers: { "Content-Type": "application/x-www-form-urlencoded" }, responseType: 'text' })
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  cyrlat(vehicleNumber: string) {
+    let dkn = vehicleNumber;
+
+    dkn = dkn.replace(/а/g, "A");
+    dkn = dkn.replace(/б/g, "B");
+    dkn = dkn.replace(/в/g, "B");
+    dkn = dkn.replace(/г/g, "");
+    dkn = dkn.replace(/д/g, "");
+    dkn = dkn.replace(/е/g, "E");
+    dkn = dkn.replace(/ж/g, "B");
+    dkn = dkn.replace(/з/g, "");
+    dkn = dkn.replace(/и/g, "");
+    dkn = dkn.replace(/й/g, "");
+    dkn = dkn.replace(/к/g, "K");
+    dkn = dkn.replace(/л/g, "");
+    dkn = dkn.replace(/м/g, "M");
+    dkn = dkn.replace(/н/g, "H");
+    dkn = dkn.replace(/о/g, "O");
+    dkn = dkn.replace(/п/g, "P");
+    dkn = dkn.replace(/р/g, "P");
+    dkn = dkn.replace(/с/g, "C");
+    dkn = dkn.replace(/т/g, "T");
+    dkn = dkn.replace(/у/g, "Y");
+    dkn = dkn.replace(/ф/g, "");
+    dkn = dkn.replace(/х/g, "X");
+    dkn = dkn.replace(/ц/g, "C");
+    dkn = dkn.replace(/ч/g, "");
+    dkn = dkn.replace(/ш/g, "");
+    dkn = dkn.replace(/щ/g, "");
+    dkn = dkn.replace(/ъ/g, "");
+    dkn = dkn.replace(/ь/g, "");
+    dkn = dkn.replace(/ю/g, "");
+    dkn = dkn.replace(/я/g, "");
+
+    dkn = dkn.replace(/А/g, "A");
+    dkn = dkn.replace(/Б/g, "B");
+    dkn = dkn.replace(/В/g, "B");
+    dkn = dkn.replace(/Г/g, "");
+    dkn = dkn.replace(/Д/g, "");
+    dkn = dkn.replace(/Е/g, "E");
+    dkn = dkn.replace(/Ж/g, "B");
+    dkn = dkn.replace(/З/g, "");
+    dkn = dkn.replace(/И/g, "");
+    dkn = dkn.replace(/Й/g, "");
+    dkn = dkn.replace(/К/g, "K");
+    dkn = dkn.replace(/Л/g, "");
+    dkn = dkn.replace(/М/g, "M");
+    dkn = dkn.replace(/Н/g, "H");
+    dkn = dkn.replace(/О/g, "O");
+    dkn = dkn.replace(/П/g, "P");
+    dkn = dkn.replace(/Р/g, "P");
+    dkn = dkn.replace(/С/g, "C");
+    dkn = dkn.replace(/Т/g, "T");
+    dkn = dkn.replace(/У/g, "Y");
+    dkn = dkn.replace(/Ф/g, "");
+    dkn = dkn.replace(/Х/g, "X");
+    dkn = dkn.replace(/Ц/g, "C");
+    dkn = dkn.replace(/Ч/g, "");
+    dkn = dkn.replace(/Ш/g, "");
+    dkn = dkn.replace(/Щ/g, "");
+    dkn = dkn.replace(/Ъ/g, "");
+    dkn = dkn.replace(/Ь/g, "");
+    dkn = dkn.replace(/Ю/g, "");
+    dkn = dkn.replace(/Я/g, "");
+    return dkn.toUpperCase();
   }
 
 }
+
 
 
--- src/app/map/modal/modal.component.ts
+++ src/app/map/modal/modal.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-modal',
@@ -10,12 +12,33 @@ export class ModalComponent implements OnInit {
 
   @Input() public data: any;
   @Output() passEntry: EventEmitter<any> = new EventEmitter();
-
+  insurance: any;
   
-  constructor(public activeModal: NgbActiveModal) { }
+  constructor(public activeModal: NgbActiveModal,
+    private apiService: ApiService) { }
   
   ngOnInit(): void {
     console.log(this.data);
+    this.apiService.getInsurance(this.data.vehicle_plates_number)
+     .subscribe(data => {
+      console.log(data.slice(data.indexOf(`<div class="rwdtable" id="printresult">`),
+      data.indexOf(`<br />
+    <a href="javascript:void(0)" id="noprint"`)));
+      
+      this.insurance = data.includes(`<div class="rwdtable" id="printresult">`)
+      ? data.slice(
+        data.indexOf(`<div class="rwdtable" id="printresult">`),
+        data.indexOf(`<a href="javascript:void(0)" id="noprint"`))
+        .replace(`/bg/информационен`, "https://www2.guaranteefund.org/bg/информационен")
+        .replace(`/images/checkmark.png`, "../../../assets/images/check.png")
+        .replace(`<img src="/images/gfprint.png">`,"<hr/>")
+        : data.slice(
+          data.indexOf(`<div class="myerr" id="printresult">`),
+          data.indexOf(`<b style="color:red;">`))
+          .replace(`<img src="/images/gfprint.png">`,"<hr/>")
+          .replace(`/images/attention.png`, "../../../assets/images/uncheck.png");
+    });
+    
     
   }
 
